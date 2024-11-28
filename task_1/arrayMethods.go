d368218cd79c2dd9c0c89b83ede69763cab31f70
dik_d <dimdek96@gmail.com>
2024-11-28 21:04:18 +0500

    добавил функции indexof, some.

--- task_1/arrayMethods.go
+++ task_1/arrayMethods.go
@@ -10,3 +10,16 @@ func delete[T any](arr []T, index int) []T {
 	arr = arr[:len(arr)-1] // Обрезаем срез
 	return arr
 }
+
+func indexof[T any](arr []T, f func(T) bool) int {
+	for index, value := range arr {
+		if f(value) {
+			return index
+		}
+	}
+	return -1
+}
+
+func some[T any](arr []T, f func(T) bool) bool {
+	return indexof(arr, f) != -1
+}
--- task_1/run.go
+++ task_1/run.go
@@ -4,6 +4,18 @@ import "fmt"
 
 func Run() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	a = delete(a, 1)
+	//a = delete(a, 1)
 	fmt.Println(a)
+	fmt.Println(indexof(a, func(i int) bool {
+		return i == 5
+	}))
+	fmt.Println(indexof(a, func(i int) bool {
+		return i == 20
+	}))
+	fmt.Println(some(a, func(i int) bool {
+		return i == 5
+	}))
+	fmt.Println(some(a, func(i int) bool {
+		return i == 20
+	}))
 }
